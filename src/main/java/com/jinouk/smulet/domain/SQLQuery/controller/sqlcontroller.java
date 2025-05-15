f987dbf923760ff7d137e2702fece105b76c5e76
LimJinOuk <jinouk038@gmail.com>
2025-05-16 01:00:08 +0900

    회원가입시 중복된 이메일이라면 로그인 거부 , 강의실 추가

--- src/main/java/com/jinouk/smulet/domain/SQLQuery/controller/sqlcontroller.java
+++ src/main/java/com/jinouk/smulet/domain/SQLQuery/controller/sqlcontroller.java
@@ -5,7 +5,6 @@ import com.jinouk.smulet.domain.SQLQuery.service.getTimeTableService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +16,6 @@ public class sqlcontroller {
     private final getTimeTableService sqlservice;
 
     @GetMapping("/course{userId}")
-    @ResponseBody
     public List<getTimeTableDTO> getcoursesByUserID(@RequestParam(required = false) Integer userId ) {
         if(userId == null){
             userId = 1;

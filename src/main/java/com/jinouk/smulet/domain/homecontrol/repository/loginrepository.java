8cdc0275a7d8ce050a56321806ef44a4691b7e41
gonii <127366729+leegoeun-art@users.noreply.github.com>
2025-05-21 18:02:57 +0900

    풀 리퀘스트 병합 #40
    * 회원 탈퇴 기능 구현

--- src/main/java/com/jinouk/smulet/domain/homecontrol/repository/loginrepository.java
+++ src/main/java/com/jinouk/smulet/domain/homecontrol/repository/loginrepository.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 public interface loginrepository extends JpaRepository<user, Long>
 {
     Optional<user> findByEmail(String email);
+
+    void deleteByEmail(String email);
 }

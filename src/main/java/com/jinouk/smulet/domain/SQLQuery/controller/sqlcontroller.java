1357e4f573ab93c5f93c5a07753a0f8a62126a96
이경진 <leemmm0226@naver.com>
2025-05-21 18:21:54 +0900

    사용자에게 현재 DB에 저장된 모든 시간표의 id값을 보내는 기능

--- src/main/java/com/jinouk/smulet/domain/SQLQuery/controller/sqlcontroller.java
+++ src/main/java/com/jinouk/smulet/domain/SQLQuery/controller/sqlcontroller.java
@@ -3,11 +3,14 @@ package com.jinouk.smulet.domain.SQLQuery.controller;
 import com.jinouk.smulet.domain.SQLQuery.dto.getTimeTableDTO;
 import com.jinouk.smulet.domain.SQLQuery.service.getTimeTableService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +28,20 @@ public class sqlcontroller {
         else
         {
             return sqlservice.getcoursesByUserID(userId);
+        }
+    }
+
+    @GetMapping("/my_page1")
+    public ResponseEntity<Map<Integer, List<Integer>>> send_tableID_count(@RequestParam(required = false) Integer userId )
+    {
+        if (userId == null)
+        {
+            throw new IllegalArgumentException("user id is null");
+        }
+        else
+        {
+            Map<Integer, List<Integer>> map = sqlservice.find_tableIDs(userId);
+            return ResponseEntity.ok(map);
         }
     }
 }
--- src/main/java/com/jinouk/smulet/domain/SQLQuery/service/getTimeTableService.java
+++ src/main/java/com/jinouk/smulet/domain/SQLQuery/service/getTimeTableService.java
@@ -4,7 +4,10 @@ import com.jinouk.smulet.domain.SQLQuery.dto.getTimeTableDTO;
 import com.jinouk.smulet.domain.SQLQuery.repository.gettimeTableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,13 @@ public class getTimeTableService {
     public List<getTimeTableDTO> getcoursesByUserID(int userId)
     {
         return timetablerepository.findCoursesByUserId(userId);
+    }
+
+    public Map<Integer, List<Integer>> find_tableIDs(Integer userId)
+    {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> list = timetablerepository.findTableIdsByUserId(userId);
+        map.put(userId, list);
+        return map;
     }
 }

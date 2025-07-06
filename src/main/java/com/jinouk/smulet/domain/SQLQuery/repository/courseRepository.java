0d231a233e0b7b5e4c7ffd9d159e0d10d9c239e4
이경진 <leemmm0226@naver.com>
2025-07-06 14:50:25 +0900

    전체 과목 리스트 반환 - 레포지토리 누락

---
+++ src/main/java/com/jinouk/smulet/domain/SQLQuery/repository/courseRepository.java
@@ -0,0 +1,7 @@
+package com.jinouk.smulet.domain.SQLQuery.repository;
+
+import com.jinouk.smulet.domain.SQLQuery.entity.course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface courseRepository extends JpaRepository<course, Long> {
+}
